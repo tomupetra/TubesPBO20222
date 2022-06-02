8941ac7490e8894341bbd3c9a158d60108cfe620
ndzaa <95361450+ndzaa@users.noreply.github.com>
2022-06-02 14:40:17 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,6 +2,14 @@ import pygame, sys
 from button import Button
 
 pygame.init()
+pygame.mixer.pre_init()
+pygame.mixer.init()
+import os
+
+# start playing the background music
+pygame.mixer.music.load(os.path.join(os.getcwd(), 'BackGroundMusic_Menu.mp3'))
+pygame.mixer.music.set_volume(0.8)
+pygame.mixer.music.play(-1)
 
 win = pygame.display.set_mode((700, 500))
 pygame.display.set_caption("B'Pong")
@@ -34,12 +42,17 @@ def main_menu():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if arena1.checkForInput(mouse_pos):
+                    pygame.mixer.Channel(1).play(pygame.mixer.Sound("sfx_button_clik.wav"), maxtime=2000)
+                    pygame.mixer.music.pause()
                     import game-1
                     game-1.run = True
                 if arena2.checkForInput(mouse_pos):
+                    pygame.mixer.Channel(1).play(pygame.mixer.Sound("sfx_button_clik.wav"), maxtime=2000)
+                    pygame.mixer.music.pause()
                     import game-2
                     game-2.run = True
                 if petunjuk.checkForInput(mouse_pos):
+                    pygame.mixer.Channel(1).play(pygame.mixer.Sound("sfx_button_clik.wav"), maxtime=2000)
                     func_petunjuk()
 
         pygame.display.update()
